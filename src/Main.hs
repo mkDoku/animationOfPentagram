8744ce02ee49b4a6f0098f753cc030bd02873732
mkDoku <bubecksascha@t-online.de>
2020-12-21 13:55:00 +0100

    Circle with 'Dots'
    - Animation now draws circle in five steps. Each section ends with a 'dot'.
    - Changed viewport
    - Added 'linear' and 'text' for next steps

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,117 @@
 #!/usr/bin/env stack
 -- stack runghc --package reanimate
+{-# LANGUAGE OverloadedStrings #-}
 module Main ( main ) where
 
 import           Reanimate
 import           Reanimate.Builtin.Documentation
+import           Reanimate.Transition
 
+import           Linear.Metric
+import           Linear.V2
+import           Linear.Vector
+
+import           Data.Text                       (Text)
+import qualified Data.Text                       as T
+
+
+ratio = 16/9
 
 main :: IO ()
 main = reanimate
   $ docEnv
+  $ mapA (withViewBox ((-1.5*ratio),-1.5,(3*ratio),3))
+  $ chainT andThen [
+        drawDotAni 0
+      ,  aniCircle 0
+      ,  drawDotAni 72
+      , aniCircle 72
+      ,  drawDotAni 144
+      , aniCircle 144
+      ,  drawDotAni 216
+      , aniCircle 216
+      ,  drawDotAni 288
+      , aniCircle 288
+                   ]
+--  $ (unitCircle `parA` rotatingStar)
+
+drawDotAni :: Double -> Animation
+drawDotAni r = staticFrame 1 translatedCircle
+  where
+    translatedCircle = translate coordX coordY (mkCircle 0.01)
+    (coordX, coordY) = fromPolarU r
+
+aniCircle :: Double -> Animation
+aniCircle r = signalA (fromToS 0.0 0.2) $ rotatedUnitCircle r
+
+scence01 :: Scene s ()
+scence01 = do
+  play $ (signalA (fromToS 0.0 0.2) $ rotatedUnitCircle 0)
+  wait 1
+
+scence02 :: Scene s ()
+scence02 = do
+  play $ (signalA (fromToS 0.0 0.2) $ rotatedUnitCircle (-72))
+  wait 1
+
+meinScene :: Scene s ()
+meinScene = do
+  newSpriteSVG $ staticStar
+  -- fork $ play (staticFrame 3 staticStar)
+  fork $ play $
+    andThen
+      (signalA (fromToS 0.0 0.2) $ rotatedUnitCircle 0)
+      (signalA (fromToS 0.0 0.2) $ rotatedUnitCircle 72)
+  wait 1
+  -- draw point
+  wait 1
+  -- continue drawing
+
+
 --  $ staticFrame 1
 --  $ staticStar
-  $ pauseAtEnd 1
-  $ rotatingStar
+--  $ pauseAtEnd 1
+--  $ rotatingStar
 
+customScaling = 1
+
+unitCircle :: Animation
+unitCircle =
+  mkAnimation 3
+--    staticFrame 3
+  $ \t ->
+    partialSvg t
+    $ pathify
+--    $ scaleToHeight 1
+      $ scaleToHeight (customScaling * 1.05)
+--    $ center
+      $ withStrokeWidth 0.001
+      $ rotate 0
+      $ flipXAxis
+      $ mkCircle 1
+
+rotatedUnitCircle :: Double -> Animation
+rotatedUnitCircle r =
+  mkAnimation 3
+--    staticFrame 3
+  $ \t ->
+    partialSvg t
+    $ pathify
+  --    $ scaleToHeight (customScaling * 1.05)
+      $ withStrokeWidth 0.01
+      $ rotate r
+      $ flipXAxis
+      $ mkCircle 1
 
 rotatingStar :: Animation
 rotatingStar =
   mkAnimation 3
   $ \t ->
       rotate (360 * t)
-    $ center
-    $ scaleToHeight 4 staticStar
+--    $ center
+--    $ scaleToHeight 1
+--    $ scaleToHeight customScaling
+    $ staticStar
 
 -- A static 'SVG' by using 'mkLinePathClosed'
 --
@@ -29,7 +119,9 @@ rotatingStar =
 -- be connected to the first point
 staticStar :: SVG
 staticStar =
-  mkLinePathClosed coordsInCartesianU
+  withStrokeWidth 0.001
+  $ scaleToHeight customScaling
+  $ mkLinePathClosed coordsInCartesianU
 
 fromDegrees :: Floating a => a -> a
 fromDegrees deg = deg * pi / 180
@@ -37,8 +129,8 @@ fromDegrees deg = deg * pi / 180
 fromPolar :: Floating a => a -> a -> (a, a)
 fromPolar r phi = (x, y)
   where
-    x = r * sin( fromDegrees phi )
-    y = r * cos( fromDegrees phi )
+    x = r * cos( fromDegrees phi )
+    y = r * sin( fromDegrees phi )
 
 fromPolarU :: Floating a => a -> (a, a)
 fromPolarU = fromPolar 1
